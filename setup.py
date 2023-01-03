cf27db0489075ef166c3b2cffb2248c3812d657f
adhukka477 <alishandhukka@gmail.com>
2023-01-03 12:44:08 -0600

    update verision on setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='sr_calculator',
-    version='0.0.1',
+    version='0.0.2',
     author='Alishan Dhukka',
     author_email='alishandhukka@gmail.com',
     description='Tool for calculating key support and resistance levels',
